ab6265473d1807a6fce6d7a5598efd64bffecf1b
2001817 <2001817@apps.nsd.org>
2025-06-03 16:43:37 +0000

    stuff

--- public/more code.js
+++ public/more code.js
@@ -1,8 +1,34 @@
-var list = [];
-
 //var temp = document.getElementById("");
 //var target = document.getElementById("");
-
 //var card = temp.querySelector("");
 
-//for(){}
+class Card{
+    constructor(num){
+        this.id = num;
+        this.suit = num;
+        this.value = num;
+        
+    }
+    getsuit(){return suit}
+    getdom(){return document.createElement()}
+}
+
+function createDeck(num){
+    var deck = [];
+
+    for(var i = 0; i < num; i++){deck.push(i)}
+
+    return deck
+}
+
+function shuffleDeck(deck){
+    for (var i = 0; i < deck.length; i++){
+        //swap places of lots of cards (at random)
+        var rng = Math.floor(Math.random() * deck.length);
+        var card1 = deck[i];
+        var card2 = deck[rng];
+        deck[i] = card2;
+        deck[rng] = card1;
+        
+    }
+}
